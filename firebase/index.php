a54b00109260947053f3a9c7d6b1ac50b73a87b9
Mona subaih <m.subaih@rozn.org>
2018-04-25 12:07:36 +0300

    ascsac

--- firebase/index.php
+++ firebase/index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+die(echo phpversion());
 header('Content-Type: application/json');
 
 //initialize request  to create order with wehook

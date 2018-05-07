545ef0ac468fd1d6aa0066332896a79025112c3c
Hussam K, Rozn <h.kurd@rozn.org>
2018-05-07 15:28:04 +0300

    updated

--- getProductByCustomerOrOrder.php
+++ getProductByCustomerOrOrder.php
@@ -4,43 +4,81 @@ include 'slackWebHooks/settings.php';
 $data = json_decode($data, true);
 
 $ordersURL = "{$mainURLEndPoint}/orders.json";
-$customersURL = "{$mainURLEndPoint}/customers.json?query=";
+$customersURL = "{$mainURLEndPoint}/customers/search.json?query=";
 
-$retriveKeyValue = intval($_GET['idd']);
+define('PAY_ACTION', 1);
+define('CANCEL_ACTION', 2);
+
+$retriveKeyValue = ($_GET['idd']);
+$actionKey = ($_GET['pay']);
+
+$ordersFilter = "financial_status=pending&fulfillment_status=unshipped&name=";
+
+function getOrdersByLink($link) {
+    $request = new Request($link);
+    $response = $request->execute();
+    if (array_key_exists('orders', $response) && sizeof($response["orders"]) > 0) {
+        return $response['orders'];
+    } else {
+        //no orders found
+        return false;
+    }
+}
+
+function getPreparedOrderInformation($order) {
+    $preparedOrder['order_number'] = $order['order_number'];
+    $preparedOrder['customer']["first_name"] = $order['customer']["first_name"];
+    $preparedOrder['customer']["last_name"] = $order['customer']["last_name"];
+    $preparedOrder['customer']["phone"] = ($order['customer']["phone"] == null) ? $order['customer']["phone"] : $order['customer']["default_address"]["phone"];
+    $preparedOrder["subtotal_price"] = $order["subtotal_price"];
+    $itemTitles = '';
+    $itemsSize = sizeof($order["line_items"]);
+    $i = 0;
+    foreach ($order["line_items"] as $lineItem) {
+        $itemTitles .= $lineItem['title'] . ' ' . '(' . $lineItem['price'] . ') ' . ($itemsSize == ($i - 1)) ? '' : ',';
+        $i++;
+    }
+    $preparedOrder["line_items"] = $itemTitles;
+
+    return $preparedOrder;
+}
+
 $numberOfDigits = strlen((string) $retriveKeyValue);
 if ($numberOfDigits < 8) {
     //get orders path
-    $params = "?status=any&name=";
-    $request = new Request($ordersURL . $params . $ordersNum);
-    $response = $request->execute();
+    $params = "?" . $ordersFilter . $retriveKeyValue;
+    $requestedOrder = '';
 
-    if (array_key_exists('orders', $response) && sizeof($response["orders"]) > 0) {
-        $orders = $response['orders'];
-        var_dump($orders);
-    } else {
-        //no orders found
+    $orders = getOrdersByLink($ordersURL . $params . $ordersNum);
+    foreach ($orders as $order) {
+        if ($order['order_number'] == $ordersNum) {
+            $requestedOrder = $ordersNum;
+        }
+    }
+    if (!$orders || $requestedOrder == '') {
         echo 'No orders found for ' . $retriveKeyValue;
     }
+    else{
+        return getPreparedOrderInformation([$requestedOrder]);
+    }
 } else {
-    //get customer path
-    $customersURL = $customersURL . 'email:0599059940@loqta.ps';
-    echo $customersURL;
-    $request = new Request($customersURL);
+    $email = $retriveKeyValue . '@loqta.ps';
+    $requestedCustomer = '';
 
+    //get customer path
+    $customersURL = $customersURL . 'email:' . $email . ';fields=id,email,phone';
+    $request = new Request($customersURL);
     $response = $request->execute();
     if (array_key_exists('customers', $response) && sizeof($response["customers"]) > 0) {
-        $customer = $response["customers"][0];
-                var_dump($response);
-
-        $customerID = $customer["id"];
-        $request = new Request($ordersURL . "?customer_id=" . $customerID);
-        $response = $request->execute();
-        if (array_key_exists('orders', $response) && sizeof($response["orders"]) > 0) {
-            $orders = $response['orders'];
-            var_dump($orders);
-
-        } else {
-            //no orders found
+        $customers = $response["customers"];
+        foreach ($customers as $customer) {
+            if ($customer['email'] == $email) {
+                $requestedCustomer = $customer;
+            }
+        }
+        $customerID = $requestedCustomer["id"];
+        $orders = getOrdersByLink($ordersURL . "?customer_id=" . $customerID . '&' . $ordersFilter);
+        if (!$orders || $requestedCustomer == '') {
             echo 'No orders found for ' . $customer['first_name'];
         }
     } else {
@@ -48,33 +86,3 @@ if ($numberOfDigits < 8) {
         echo 'No customer name found for id = ' . $retriveKeyValue;
     }
 }
-
-
-//$mainURL = $mainURLEndPoint . "/orders/" . $data['id'] . "/metafields.json";
-//
-//if ($data['cancelled_at'] != '') {
-//    $key = 'cancel_date';
-//} 
-//elseif ($data['financial_status'] == 'paid') {
-//    $key = 'paid_date';
-//}
-//elseif ($data['fulfillment_status'] == 'fulfilled') {
-//    $key = 'fulfill_date';
-//}
-//
-//$metaFieldData = array(
-//    'metafield' => array(
-//        'namespace' => 'operations',
-//        'key' => $key,
-//        'value' => date('d/m/Y h:i:s A'),
-//        "value_type" => "string"
-//    )
-//);
-//$headers = array(
-//    'Content-Type:application/json'
-//);
-//$request = new Request($mainURL, 'POST', json_encode($metaFieldData));
-//$request->setHeaders($headers);
-//$result = $request->execute(false);
-
-
